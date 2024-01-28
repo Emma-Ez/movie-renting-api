8b415980702bcc5e98e70f7849770ffb1d47bbc3
Emma-Ez <emma.favoured@yahoo.com>
2024-01-28 23:50:42 +0100

    Customer Class added

--- movie.js
+++ movie.js
@@ -7,9 +7,49 @@ class Movie {
       this.availableCopies = availableCopies;
       this.rentedBy = null;
     }
-  // ... (rent and returnMovie methods)
+    
+  rent(customer) {
+    if (this.availableCopies > 0) {
+      this.availableCopies--;
+      this.rentedBy = customer;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  returnMovie() {
+    this.availableCopies++;
+    this.rentedBy = null;
+  }
   getAvailability() {
     return this.availableCopies > 0;
   }
 }
 
+// Customer class
+class Customer {
+    constructor(name, email) {
+      this.name = name;
+      this.email = email;
+      this.rentals = [];
+    }
+  
+    rentMovie(movie) {
+      if (movie.rent(this)) {
+        this.rentals.push(movie);
+        return true;
+      } else {
+        return false;
+      }
+    }
+  
+    returnMovie(movie) {
+      movie.returnMovie();
+      this.rentals.remove(movie);
+    }
+    viewRentalHistory() {
+        console.log("Rental History for", this.name);
+        this.rentals.forEach(movie => console.log(movie.title));
+    }
+  }
